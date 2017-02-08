347d0f272cc3f48370fc9a17a24bd7914fce2e7c
zimski <chadda.chakib@gmail.com>
2017-02-08 16:01:57 +0100

    Add a filter to process cover image

--- scripts/resize_covers.sh
+++ scripts/resize_covers.sh
@@ -4,8 +4,12 @@ FILES=`find ../assets/images/post_cover/*jpg | grep -v resized`
 
 for file in $FILES
 do
+    echo "Processing image: $file"
+    bash ./textcleaner -g -e stretch -f 60 -o 3 -s 1  $file $file
+    echo "Processing Image done"
     echo "Resizing Image: $file"
     convert $file -resize 560x300 ${file/.jpg/_resized.jpg}
     echo "Resizing Image done"
+
     rm $file
 done
