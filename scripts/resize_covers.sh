86c9617a52a25d1362ce81d64ad95b5d954ea643
chadda chakib <chadda.chakib@gmail.com>
2018-03-17 20:31:17 +0100

    Force the resizing to bypass the ratio

--- scripts/resize_covers.sh
+++ scripts/resize_covers.sh
@@ -8,7 +8,7 @@ do
     bash ./textcleaner -g -e stretch -f 60 -o 3 -s 1  $file $file
     echo "Processing Image done"
     echo "Resizing Image: $file"
-    convert $file -resize 560x300 ${file/.jpg/_resized.jpg}
+    convert $file -resize "560x300!" ${file/.jpg/_resized.jpg}
     echo "Resizing Image done"
 
     rm $file
